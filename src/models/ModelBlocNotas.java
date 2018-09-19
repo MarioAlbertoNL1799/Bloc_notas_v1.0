d01d535ab4f8c674d1b5e138f42b1711fee48549
MarioAlbertoNL1799 <37378931+MarioAlbertoNL1799@users.noreply.github.com>
2018-09-18 22:59:46 -0500

    Creacion de aplicacion para abrir archivo txt, leerlo y agregar lineas por medio del modelo MVC

---
+++ src/models/ModelBlocNotas.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author manl_
+ */
+public class ModelBlocNotas {
+    private String path = "C:\\archivos\\archivo.txt";
+    private String linea = "";
+    private String message;
+    
+    public void readFile(){
+    try{
+        String row;
+        try(FileReader file = new FileReader(path)){
+            BufferedReader bufferedReader = new BufferedReader(file);
+            while ((row = bufferedReader.readLine()) != null){
+                linea = linea +  row + "\n";
+                System.out.println(row);
+            }
+            bufferedReader.close();
+        }
+    }
+    catch(FileNotFoundException err){
+        System.err.println("Archivo no encontrado: "+err.getMessage());
+        }
+    catch(IOException err){
+        System.err.println("Errior en la operacion de entrada/salida: "+err.getMessage());
+        }
+    }   
+
+    public void writeFile(){
+        try{
+            File file = new File(path);
+            FileWriter fileWriter = new FileWriter(file,true);
+            try (PrintWriter printWriter = new PrintWriter(fileWriter)){
+                printWriter.println(message);
+                printWriter.close();
+            }
+        }
+        catch(FileNotFoundException err){
+            System.err.println("Archivo no encontrado: "+err.getMessage());
+        }
+        catch(IOException err){
+            System.err.println("Errior en la operacion de entrada/salida: "+err.getMessage());
+        }
+    }
+    
+    public String getPath() {
+        return path;
+    }
+
+    private void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getLinea() {
+        return linea;
+    }
+
+    public void setLinea(String linea) {
+        this.linea = linea;
+    }
+}
